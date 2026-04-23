ddaa9f79de6afc59057acccdf180cfb449ea73eb
Alan Diep <alan@macbookpro.mynetworksettings.com>
2026-04-22 21:19:17 -0400

    feat: add RGB gradient effect

--- donut.c
+++ donut.c
@@ -9,12 +9,13 @@ int main() {
     int k;
     float z[1760];
     char b[1760];
+    float t_color = 0;
     printf("\x1b[2J");
     for (;;) {
         memset(b, 32, 1760);
         memset(z, 0, 7040);
-        for (j = 0; j < 6.28318; j += 0.07) {
-            for (i = 0; i < 6.28318; i += 0.02) {
+        for (j = 0; j < 6.28; j += 0.07) {
+            for (i = 0; i < 6.28; i += 0.02) {
                 float c = sin(i);
                 float d = cos(j);
                 float e = sin(A);
@@ -30,18 +31,29 @@ int main() {
                 int y = 12 + 15 * D * (l * h * n + t * m);
                 int o = x + 80 * y;
                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-                if (22 > y && y >= 0 && x > 0 && 80 > x && D > z[o]) {
+                if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
                     z[o] = D;
                     b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
                 }
             }
         }
         printf("\x1b[H");
-        for (k = 0; k < 1761; k++) {
-            putchar(k % 80 ? b[k] : 10);
-            A += 0.00004;
-            B += 0.00002;
+        for (k = 0; k < 1760; k++) {
+            if (k % 80 == 0 && k != 0) putchar(10);
+            if (b[k] != ' ') {
+                int x = k % 80;
+                int y = k / 80;
+                int r = 127.5 * (sin(0.1 * x + t_color) + 1);
+                int g = 127.5 * (sin(0.1 * y + t_color + 2) + 1);
+                int b_val = 127.5 * (sin(0.1 * (x + y) + t_color + 4) + 1);
+                printf("\x1b[38;2;%d;%d;%dm%c\x1b[0m", r, g, b_val, b[k]);
+            } else {
+                putchar(' ');
+            }
         }
+        A += 0.04;
+        B += 0.02;
+        t_color += 0.05;
         usleep(30000);
     }
     return 0;
